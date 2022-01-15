e465f5aaca0edf6965042ac21a71347838a60cf3
sampath-enquero <kolisetty.jayasampath@enquero.com>
2022-01-15 22:16:48 +0530

    initial commit in cache

--- config-service-mongo/src/main/java/com/practice/symphony/configservice/service/ConfigService.java
+++ config-service-mongo/src/main/java/com/practice/symphony/configservice/service/ConfigService.java
@@ -1,25 +1,37 @@
 package com.practice.symphony.configservice.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.practice.symphony.configservice.dao.ConfigDao;
 import com.practice.symphony.configservice.entity.Config;
 
+@Service
 public class ConfigService {
 	
 	@Autowired
 	private ConfigDao configDao;
 	
+	Logger logger = LoggerFactory.getLogger(ConfigService.class);
+	
 	private CacheService cacheService;
 	
 	public Config getConfig(int tenantId) {
-		Object configFromCache = cacheService.getConfig(tenantId);
+		//Object configFromCache = cacheService.getConfig(tenantId);
 		
-		if(configFromCache!=null) {
-			return (Config) configFromCache;
-		}
+//		if(configFromCache!=null) {
+//			return (Config) configFromCache;
+//		}
 		
-		return configDao.getConfig(tenantId);
+		logger.info("Expired in cache, getting from mongo and adding to cache again");
+		
+		Config configFromMongo = configDao.getConfig(tenantId);
+		
+		cacheService.addConfigToCache(tenantId, configFromMongo);
+		
+		return configFromMongo;
 	}
 	
 	public Config updateConfig(Config newConfig) {
